00875a33cc1b5b72ab89c4857e88e647a988ce8b
walter <wmoncada@tecsup.edu.pe>
2023-10-17 15:25:49 -0500

    17102023_1

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+   
+  const HomeScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Componentes de Flutter'),
+        elevation: 0,
+        backgroundColor: Colors.indigo,
+      ),
+      body: ListView.separated(
+        itemBuilder:(context, index) => ListTile(
+          leading: const Icon(Icons.app_settings_alt, color: Colors.indigoAccent,),
+          title: const Text('Items'),
+          trailing: const Icon(Icons.circle_notifications_sharp, color: Colors.indigoAccent),
+          onTap: () {
+          
+          },
+        ), 
+        separatorBuilder:(context, index) => const Divider(), 
+        itemCount: 20)
+    );
+  }
+}
